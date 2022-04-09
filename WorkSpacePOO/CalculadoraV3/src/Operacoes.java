1606787b7c844bc666712b25060b65ba308a4f7e
Gabriel Gonçalves <ga.goncalves159753@gmail.com>
2022-04-09 17:23:25 -0300

    POO em Java

---
+++ WorkSpacePOO/CalculadoraV3/src/Operacoes.java
@@ -0,0 +1,23 @@
+
+public class Operacoes {
+
+	public double calculoSoma(double num1, double num2) {
+		double soma = num1 + num2;
+		return soma;		
+	}
+	
+	public double calculoSubtracao(double num1, double num2) {
+		double sub = num1 - num2;
+		return sub;
+	}
+	
+	public double calculoMultiplicacao(double num1, double num2) {
+		double mult = num1 * num2;
+		return mult;
+	}
+	
+	public double calculoDivisao(double num1, double num2) {
+		double divi = num1 / num2;
+		return divi;
+	}
+}
---
+++ WorkSpacePOO/CalculadoraV3/src/Principal.java
@@ -0,0 +1,45 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	private static Operacoes oper = new Operacoes ();
+	
+	public static void main(String[] args) {
+		
+		int continuar;
+		double resultado =0;
+		
+		do {
+			double num1 = EntradaSaida.solicitaNumero("1º");
+			double num2 = EntradaSaida.solicitaNumero("2º");
+			int operacao = EntradaSaida.solicitaOperacao();
+			
+			switch(operacao) {			
+			case 1:
+				resultado = oper.calculoSoma(num1,num2);
+				break;
+			case 2:
+				resultado = oper.calculoSubtracao(num1,num2);
+				break;
+			case 3:
+				resultado = oper.calculoMultiplicacao(num1,num2);
+				break;
+			case 4:
+				while(num2==0) {
+					num2 = EntradaSaida.solicitaNumero("2º");
+				}
+				resultado = oper.calculoDivisao(num1,num2);
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Operação inválida! A calculadora será encerrada!");
+				System.exit(0);
+			}
+			
+			EntradaSaida.mostraResultado(resultado, operacao);
+			continuar = Integer.parseInt(JOptionPane.showInputDialog("Deseja continuar calculando? 1- Sim | 2- Não"));
+			
+		}while (continuar==1);
+
+	}
+	
+}
